e693a493bd70673bd209bb6611dff21f94e70f6a
rig0rmortis <omar@pigeon.works>
2020-06-29 18:18:46 -0400

    add a pretty-formatter

--- src/core.clj
+++ src/core.clj
@@ -1,15 +1,17 @@
 (ns clj-json-logger.core
   (:import java.util.Date)
   (:require [clojure.string :as str]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.pprint :as pprint]))
 
 (def config
   {:level  :debug ; one of [:debug, :info, :warn, :error, :fatal]
    :stdout true   ; log to stdout by default
    :file   nil    ; path to file if logging to file, nil means stdout only
+   :pretty true  ; whether or not we want to pretty print logs (useful for dev)
    })
 
-(def log-lvl-mapping
+(def level-mapping
   {:debug 10
    :info  20
    :warn  30
@@ -19,29 +21,47 @@
 (defn current-epoch-time []
   (.getTime (java.util.Date.)))
 
-(defn to-json [log]
-  (json/write-str log))
+(defn pretty-formatter [log]
+  ;; (apply str [(pprint/cl-format nil "level=~a namespace=~a message=\"~a\""
+  ;;                               (name (log :level))
+  ;;                               (log :namespace)
+  ;;                               (log :message))
+  ;;             (doseq [[k v] (log :kv)]
+  ;;               (pprint/cl-format nil " ~a=~a " k v))]))
+  ;; TODO: figure out why this doseq is returning a nil
+  (pprint/cl-format nil"level=~a namespace=~a message=\"~a\""
+                    (name (log :level))
+                    (log :namespace)
+                    (log :message)))
+
+(defn convert-to-string [log]
+  (if (config :pretty)
+    (pretty-formatter log)
+    (json/write-str log)))
 
 (defn write-to-stdout [log]
   (.write *out* (str log "\n")))
 
-(defn write-log [log]
-  (->> log
-       to-json
-       write-to-stdout))
+(defn- write-log [log]
+  (when (config :stdout)
+    (->> log
+         convert-to-string
+         write-to-stdout)))
 
-(defn log [lvl msg & {:keys [kv err]}]
+(defn log [level message & {:keys [kv error]}]
   (write-log (into {}
-                   [[:message msg]
-                    [:level lvl]
+                   [[:message message]
+                    [:level level]
+                    [:namespace (str *ns*)]
+                    [:level_number (level-mapping level)]
                     [:timestamp (current-epoch-time)]
-                    (when err
-                      {:error      (.toString err)
-                       :stacktrace (str/join "\n" (.getStackTrace err))})
-                    (when kv [:json kv])])))
+                    (when error
+                      {:error      (.toString error)
+                       :stacktrace (str/join "\n" (.getStackTrace error))})
+                    (when kv [:kv kv])])))
 
-(defmacro debug [msg & args] `(log :debug ~msg ~@args))
-(defmacro info  [msg & args] `(log :info  ~msg ~@args))
-(defmacro warn  [msg & args] `(log :warn  ~msg ~@args))
-(defmacro error [msg & args] `(log :error ~msg ~@args))
-(defmacro fatal [msg & args] `(log :fatal ~msg ~@args))
+(defmacro debug [message & args] `(log :debug ~message ~@args))
+(defmacro info  [message & args] `(log :info  ~message ~@args))
+(defmacro warn  [message & args] `(log :warn  ~message ~@args))
+(defmacro error [message & args] `(log :error ~message ~@args))
+(defmacro fatal [message & args] `(log :fatal ~message ~@args))
